8c4ff4f2366cef2a2cb76291d33d1afd4b2d573f
Ram30001 <ram30661@gmail.com>
2022-06-13 12:06:16 -0500

    started left side of choose your own adventure

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Your Code here
+let firstAnswer = window.prompt("Do you head left or right")
+console.log(firstAnswer)
+
+if(firstAnswer === "left"){
+    let secondAnswer = window.prompt("You come across a stray cat.  It scampers off down a small hole, just large enough for you to crawl through.  Do you follow it, or continue on your path?");
+    
+    if(secondAnswer === "follow"){let thirdAnswer = window.prompt("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsistin off of inexplicably warn soup.  They are content with you staying, but you wonder if you should alert the world to this magical safe haven.  Do you stay or spread");
+
+    if(thirdAnswer === "stay"){let fourthAnswer = window.prompt("in or out");
+        if(fourthAnswer === "in"){window.alert("you have completed the adventur")}}}}
+    
+    else if(secondAnswer === "continue"){let thirdAnswer = window.prompt("You come across a chamber. blah blah blah.  Take staircase or ladder")}
+    
+    
+
+
+
+
+
