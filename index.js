4d2dff9a2fab5ce6e2db78fa6abfdc6be6b4d898
Ram30001 <ram30661@gmail.com>
2022-06-15 13:10:27 -0500

    added continue option to second answer choices

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ console.log(firstAnswer)
 if(firstAnswer === "left"){
     let secondAnswer = window.prompt("You come across a stray cat.  It scampers off down a small hole, just large enough for you to crawl through.  Do you follow it, or continue on your path?");
     
-    if(secondAnswer === "follow"){let thirdAnswer = window.prompt("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsistin off of inexplicably warn soup.  They are content with you staying, but you wonder if you should alert the world to this magical safe haven.  Do you stay or spread");
+    if(secondAnswer === "follow"){
+        let thirdAnswer = window.prompt("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsistin off of inexplicably warm soup.  They are content with you staying, but you wonder if you should alert the world to this magical safe haven.  Do you stay or spread");
    
-       if(thirdAnswer === "stay"){let fourthAnswer = window.prompt("in or out");
-        if(fourthAnswer === "in"){window.alert("you have completed the adventur")}}}}
+       if(thirdAnswer === "stay"){
+        let fourthAnswer = window.prompt("in or out");
+        
+        if(fourthAnswer === "in"){window.alert("you have completed the adventure")}}}
     
-    else if(secondAnswer === "continue"){let thirdAnswer = window.prompt("You come across a chamber. blah blah blah.  Take staircase or ladder");
+    else if(secondAnswer === "continue"){
+        let thirdAnswer = window.prompt("You come across a chamber. blah blah blah.  Take staircase or ladder")}}
 
     
-else(firstAnswer === "right"){
-    let secondAnswer = window.prompt("blah blah blah.  yes or not"}
-    
+
     
 
 
